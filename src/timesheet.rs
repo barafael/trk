9ab5d8956b594e4e36d5724d5af6ac6b485b38d4
Rafael Bachmann <triolenzunge@gmail.com>
2017-03-21 21:06:54 +0100

    add event enum

--- src/timesheet.rs
+++ src/timesheet.rs
@@ -3,9 +3,23 @@ extern crate time;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
+enum Event {
+    WeekBegin,
+    WeekEnd,
+    SessionBegin,
+    SessionEnd,
+    Break,
+    Continue,
+    Meta { text: String },
+    Commit { hash: u64 },
+    Branch { text: String },
+}
+
+#[derive(Debug)]
 pub struct Session {
     pub id: u64,
     pub user: &'static str,
+    events: Vec<Event>
 }
 
 impl Session {
@@ -15,6 +29,7 @@ impl Session {
         Session {
             id: seconds,
             user: "Rafael",
+            events: Vec::<Event>::new(),
         }
     }
 }
