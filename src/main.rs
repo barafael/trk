80c20d78951a3b2812d30bdcb04e1e7eacf89195
Rafael Bachmann <triolenzunge@gmail.com>
2017-03-22 09:39:36 +0100

    add clap arg handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,64 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 mod timesheet;
 
 fn main() {
-    let ts = timesheet::Session::new();
+    let mut ts = timesheet::Session::new();
+
+    let matches = clap_app!(myapp =>
+        (version: "0.1")
+        (author: "Rafael B. <mediumendian@gmail.com>")
+        (about: "Create timesheets from git history and meta info")
+            (@arg CONFIG: -c --config +takes_value "[UNUSED] Sets a custom config file")
+            (@arg debug: -d ... "Sets the level of debugging information")
+            (@subcommand test =>
+                (about: "controls testing features")
+                (version: "0.0")
+                (author: "Someone E. <someone_else@other.com>")
+                (@arg verbose: -v --verbose "Print test information verbosely")
+                (@arg sayhello: -s --say "Greet")
+            )
+            (@subcommand begin =>
+                (about: "Begin Session or Week")
+                (version: "0.1")
+                (author:  "Rafael B. <mediumendian@gmail.com>")
+                (@subcommand week =>
+                    (about: "Begin week")
+                    (version: "0.1")
+                    (author:  "Rafael B. <mediumendian@gmail.com>")
+                )
+                (@subcommand session =>
+                    (about: "Begin session")
+                    (version: "0.1")
+                    (author:  "Rafael B. <mediumendian@gmail.com>")
+                )
+            )
+        )
+            .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'trk -v -v -v' or 'trk -vvv' vs 'trk -v'
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("begin") {
+        if matches.is_present("week") {
+            ts.append_event(timesheet::Event::WeekBegin)
+        } else if matches.is_present("session") {
+            ts.append_event(timesheet::Event::SessionBegin)
+        }
+    }
     println!("{:?}", ts);
 }
--- src/timesheet.rs
+++ src/timesheet.rs
@@ -3,7 +3,7 @@ extern crate time;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
-enum Event {
+pub enum Event {
     WeekBegin,
     WeekEnd,
     SessionBegin,
@@ -12,14 +12,14 @@ enum Event {
     Continue,
     Meta { text: String },
     Commit { hash: u64 },
-    Branch { text: String },
+    Branch { name: String },
 }
 
 #[derive(Debug)]
 pub struct Session {
     pub id: u64,
     pub user: &'static str,
-    events: Vec<Event>
+    events: Vec<Event>,
 }
 
 impl Session {
@@ -30,6 +30,25 @@ impl Session {
             id: seconds,
             user: "Rafael",
             events: Vec::<Event>::new(),
+        }
+    }
+    pub fn append_event(&mut self, e: Event) {
+        match e {
+            Event::WeekBegin => {
+                println!("Week begin!");
+                self.events.push(e)
+            }
+            Event::WeekEnd => println!("Week end!"),
+
+            Event::SessionBegin => println!("Session begin!"),
+            Event::SessionEnd => println!("Session end!"),
+
+            Event::Break => println!("Break begin!"),
+            Event::Continue => println!("Continue work"),
+
+            Event::Meta { text } => println!("Meta: {}", text),
+            Event::Commit { hash } => println!("Commit hash: {}", hash),
+            Event::Branch { name } => println!("Change to branch {}", name),
         }
     }
 }
