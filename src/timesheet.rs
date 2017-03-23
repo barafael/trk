022ac1e0b8663f6cf216cd130323755c61dd3d8d
Rafael Bachmann <triolenzunge@gmail.com>
2017-03-23 10:19:59 +0100

    add period struct and impl

--- src/timesheet.rs
+++ src/timesheet.rs
@@ -18,6 +18,7 @@ pub enum Event {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Session {
     pub id: u64,
+    // is this field necessary?
     pub user: String,
     events: Vec<Event>,
 }
@@ -32,7 +33,7 @@ impl Session {
             events: Vec::<Event>::new(),
         }
     }
-    pub fn append_event(&mut self, e: Event) {
+    pub fn push_event(&mut self, e: Event) {
         match e {
             Event::BeginPeriod => self.events.push(e),
             Event::EndPeriod => self.events.push(e),
@@ -44,5 +45,30 @@ impl Session {
             Event::Commit { .. } => self.events.push(e),
             Event::Branch { .. } => self.events.push(e),
         }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Period {
+    id: u64,
+    // unused field is pointless
+    user: String,
+    sessions: Vec<Session>,
+}
+
+impl Period {
+    pub fn new() -> Period {
+        let now = SystemTime::now();
+        let seconds = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
+        Period {
+            id: seconds,
+            user: "Rafael".to_string(),
+            sessions: Vec::<Session>::new(),
+        }
+    }
+
+    pub fn push_session(&mut self, s: Session) {
+        // Checking for valid session here
+        self.sessions.push(s);
     }
 }
